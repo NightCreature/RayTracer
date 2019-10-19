a4fd812484351cda6260e3cbd1c9a632e6e5acb4
Paul Kruitz <paulkruitz@gmail.com>
2019-10-19 15:28:29 -0400

    Fixed collision test for spheres

--- RayTracer/Shape.cpp
+++ RayTracer/Shape.cpp
@@ -8,42 +8,11 @@
 bool Sphere::Intersect(const Ray& ray, IntersectionInformation& info)
 {
     auto originToCenter = m_position - ray.m_origin;
-    auto cosOriginToCenter = originToCenter.dot(ray.m_direction);
-    if (cosOriginToCenter > 0) //Might have an intersection
-    {
-        auto projectionOfCenterOnRay = cosOriginToCenter * ray.m_direction;
-        auto length = projectionOfCenterOnRay.length();
-        if (length <= m_radius)
-        {
-            //intersection possible
-            if (length == m_radius)
-            {
-                //Found the point
-                info.m_hit = projectionOfCenterOnRay;
-            }
-            else
-            {
-                //Need to find p1, closest hit on the ray.
-                auto centerToPc = projectionOfCenterOnRay - m_position;
-                auto distance = sqrt(m_radius* m_radius - centerToPc.dot(centerToPc)); 
-                auto factorOfIntersection = 0.0;
-                if (length > distance)
-                {
-                    factorOfIntersection = (projectionOfCenterOnRay - ray.m_origin).length() - distance;
-                }
-                else
-                {
-                    factorOfIntersection = (projectionOfCenterOnRay - ray.m_origin).length() + distance;
-                }
-
-                info.m_hit = ray.m_origin + ray.m_direction * factorOfIntersection;
-            }
-        }
-
-        return true;
-    }
-
-    return false;
+    auto a = ray.m_direction.dot(ray.m_direction);
+    auto b = 2 * originToCenter.dot(ray.m_direction);
+    auto c = originToCenter.dot(originToCenter) - m_radius * m_radius;
+    auto discriminant = b * b - 4 * a * c;
+    return discriminant > 0;
 }
 
 ///-----------------------------------------------------------------------------
--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -10,15 +10,15 @@ int main()
 {
     Sphere sphere;
     sphere.m_position = Vector3::zero();
-    sphere.m_radius = 1.0;
+    sphere.m_radius = 0.5;
     Scene scene;
     scene.m_shapes.push_back(&sphere);
 
     Camera cam;
     cam.positionCamera(Vector3(0.0, 0.0, -50.0), Vector3::zero(), Vector3::yAxis());
     
-    RenderOptions renderOptions(cam, 512, 512, math::gmPI / 4.0);
-    auto maxSampleCount = 10;
+    RenderOptions renderOptions(cam, 1024, 1024, math::gmPI / 8.0);
+    auto maxSampleCount = 1;
 
     std::vector<Vector4> imagePixels;
     imagePixels.resize(renderOptions.m_outputWidth * renderOptions.m_outputHeight);
